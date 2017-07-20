67c6da4f4a8a988c90c856d406f1802adb2aaba6
Alasdair Mercer <mercer.alasdair@gmail.com>
2017-07-20 15:35:56 +0100

    completed JSDoc for design services

--- src/service/design/DribbbleService.js
+++ src/service/design/DribbbleService.js
@@ -25,14 +25,15 @@
 const HttpService = require('../HttpService');
 
 /**
- * TODO: Document
+ * An implementation of {@link HttpService} that checks whether the name is available on
+ * <a href="https://dribbble.com">Dribbble</a>.
  */
 class DribbbleService extends HttpService {
 
   /**
-   * TODO: Document
+   * Creates an instance of {@link DribbbleService} under the specified <code>category</code>.
    *
-   * @param {string} category -
+   * @param {string} category - the category to be used
    * @public
    */
   constructor(category) {
@@ -41,6 +42,7 @@ class DribbbleService extends HttpService {
 
   /**
    * @override
+   * @inheritDoc
    */
   getRequestOptions(name) {
     return { uri: `https://dribbble.com/${encodeURIComponent(name)}` };
