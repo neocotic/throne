cd58ed95c1b0a01e6f6f0c0058d767ad2fa71a58
Alasdair Mercer <mercer.alasdair@gmail.com>
2017-07-20 16:42:48 +0100

    completed JSDoc for social services

--- src/service/social/LinkedInService.js
+++ src/service/social/LinkedInService.js
@@ -25,14 +25,15 @@
 const HttpService = require('../HttpService');
 
 /**
- * TODO: Document
+ * An implementation of {@link HttpService} that checks whether the name is available on
+ * <a href="https://www.linkedin.com">LinkedIn</a>.
  */
 class LinkedInService extends HttpService {
 
   /**
-   * TODO: Document
+   * Creates an instance of {@link LinkedInService} under the specified <code>category</code>.
    *
-   * @param {string} category -
+   * @param {string} category - the category to be used
    * @public
    */
   constructor(category) {
@@ -41,6 +42,7 @@ class LinkedInService extends HttpService {
 
   /**
    * @override
+   * @inheritDoc
    */
   getRequestOptions(name) {
     return {
