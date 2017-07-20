c5d5b71538007a2c0284038698f89482a9b598a8
Alasdair Mercer <mercer.alasdair@gmail.com>
2017-07-20 16:35:41 +0100

    completed JSDoc for shop services

--- src/service/shop/EtsyService.js
+++ src/service/shop/EtsyService.js
@@ -25,14 +25,15 @@
 const HttpService = require('../HttpService');
 
 /**
- * TODO: Document
+ * An implementation of {@link HttpService} that checks whether the name is available on
+ * <a href="https://www.etsy.com">Etsy</a>.
  */
 class EtsyService extends HttpService {
 
   /**
-   * TODO: Document
+   * Creates an instance of {@link EtsyService} under the specified <code>category</code>.
    *
-   * @param {string} category -
+   * @param {string} category - the category to be used
    * @public
    */
   constructor(category) {
@@ -41,6 +42,7 @@ class EtsyService extends HttpService {
 
   /**
    * @override
+   * @inheritDoc
    */
   getRequestOptions(name) {
     return { uri: `https://www.etsy.com/people/${encodeURIComponent(name)}` };
